79a3eb5d847612bc2f296d4c259877d4174b0b97
red_pantera <az.zhenchak@gmail.com>
2019-04-14 15:35:32 +0300

    Add files via upload

---
+++ ZhenchakLab05/Views/InfoView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace ZhenchakLab05
+{
+    /// <summary>
+    /// Interaction logic for InfoView.xaml
+    /// </summary>
+    internal partial class InfoView : UserControl
+    {
+        internal InfoView(System.Diagnostics.Process process)
+        {
+            InitializeComponent();
+            DataContext = new InfoViewModel(process.Modules, process.Threads);
+        }
+    }
+}
